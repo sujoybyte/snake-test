3026d0261698fb6b7ad6286540708a0a0ae0ccdf
Sujoy Ghosh <sujoybyte@gmail.com>
2022-05-23 00:14:21 +0530

    added: controller script

---
+++ Assets/Scripts/Controller.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+
+[RequireComponent(typeof(Snake))]
+public class Controller : MonoBehaviour
+{
+    private Snake _snake;
+    private GameObject _snakeHead;
+
+
+    private void Awake()
+	{
+		_snake = GetComponent<Snake>();
+        _snakeHead = _snake.Head;
+	}
+
+	private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+            MoveSnake(Vector3.left);
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+            MoveSnake(Vector3.right);
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+            MoveSnake(Vector3.forward);
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+            MoveSnake(Vector3.back);
+    }
+
+    private void MoveSnake(Vector3 direction)
+    {
+        MoveBody(MoveHead(direction));
+    }
+
+    private Vector3 MoveHead(Vector3 direction)
+	{
+        Vector3 oldHeadPosition = _snakeHead.transform.position;
+        _snakeHead.transform.position += direction;
+
+        return oldHeadPosition;
+    }
+    private void MoveBody(Vector3 position)
+	{
+        var fullBody = _snake.FullBody;
+        var fullBodyCount = fullBody.Count;
+
+        for (int i = 0; i < fullBodyCount; i++)
+        {
+            (position, fullBody[i].position) = (fullBody[i].position, position);
+        }
+
+        _snake.LastBodyPosition = position;
+    }
+}
